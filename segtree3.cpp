0c360fd0468c70f01ff8f91b00df70e1812d11d9
Shantanu Balaji Totewad <shantanutotewad04@gmail.com>
2024-09-01 10:34:09 +0530

    add files

---
+++ segtree3.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long ;
+class segTree
+{
+    public:
+    vector<ll>seg;
+    segTree(int n){
+        seg.resize(4*n+1);
+    }
+    void build(int ind, int low, int high, ll arr[])
+    {
+        if (low == high)
+        {
+            seg[ind] = arr[low];
+            return;
+        }
+        ll mid = (low + high) >> 1;
+        build(2 * ind + 1, low, mid, arr);
+        build(2 * ind + 2, mid + 1, high, arr);
+        seg[ind] = (seg[2 * ind + 1] + seg[2 * ind + 2]);
+    }
+    ll query(int ind, int low, int high, int l, int r)
+    {
+        // no overlap
+        if (l > high || r < low)
+            return 0;
+
+        // completely overlap
+        if (l <= low && r >= high)
+            return seg[ind];
+
+        // partially overlap
+        ll mid = (low + high) >> 1;
+        ll left = query(2 * ind + 1, low, mid, l, r);
+        ll right = query(2 * ind + 2, mid + 1, high, l, r);
+
+        return (left+ right);
+    }
+    void update(int ind, int low, int high, int i, int val)
+    {
+        if (low == high)
+        {
+            seg[ind] = val;
+            return;
+        }
+        ll mid = (low + high) >> 1;
+        if (i <= mid)
+            update(2 * ind + 1, low, mid, i, val);
+        else
+            update(2 * ind + 2, mid + 1, high, i, val);
+
+        seg[ind] = (seg[2 * ind + 1] + seg[2 * ind + 2]);
+    }
+};
+
+int main(){
+    int n1,q;
+    cin >> n1 >> q;
+    ll arr1[n1];
+    for(int i=0;i<n1;i++) cin >> arr1[i];
+    segTree sg1(n1);
+    sg1.build(0,0,n1-1,arr1);
+
+    while(q--){
+        int type ;
+        cin>>type;
+        if(type == 2){
+            int l,r;
+            cin>>l>>r;
+            cout<<sg1.query(0,0,n1-1,l-1,r-1)<<endl;
+        }
+        else{
+            int k,u;
+            cin>>k>>u;
+            sg1.update(0,0,n1-1,k-1,u);
+        }
+    }
+}
